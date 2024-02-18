6fde5bb61a7a00ee13d9f292a263dba4089cdc14
mateussfortes <mateussilveirafortes@gmail.com>
2024-02-18 20:07:44 -0300

    Desenvolvendo a API

---
+++ src/controllers/Controller.js
@@ -0,0 +1,17 @@
+class Controller {
+    constructor(entidadeService) {
+        this.entidadeService = entidadeService;
+    }
+
+    async pegaTodos(req, res) {
+        try {
+            const listaDeRegistro = await this.entidadeService.pegaTodosOsRegistros();
+            return res.status(200).json(listaDeRegistro);
+        }
+        catch (erro) {
+
+        }
+    }
+}
+
+module.exports = Controller;
